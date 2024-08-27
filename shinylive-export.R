bd967e9647dd5ff4daa1ffa9eae488f0dcdea3c8
jeankwizera <jeankwizera@kwizresearch.com>
2024-08-27 20:36:44 +0000

    update 1st section on market analysis

--- shinylive-export.R
+++ shinylive-export.R
@@ -6,3 +6,11 @@ library(here)
 # - shinylive
 
 # use chunks {shinylive-r} to embed shiny app
+
+update_github = function(message) {
+  system("git add -A")
+  system(paste0('git commit -m "', message, '"'))
+  system("git push origin master:main")
+}
+
+update_github(message = "update 1st section on market analysis")
